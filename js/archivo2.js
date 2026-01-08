5edd4ca0ee37c058e7c5f11067c6401977a9f5a9
Emanuel santos cruz <240557@utxicotepec.edu.mx>
2026-01-08 14:29:27 -0600

    crear funciones y estilizado

--- js/archivo2.js
+++ js/archivo2.js
@@ -1,3 +1,33 @@
 function saludar(){
     alert("Bienvenido a la academia de T.I")
 }
+
+function suma(){
+    var valor1 =24;
+    var valor2 =13;
+    var resultado = valor1 + valor2
+    alert("El resultado de la suma es:" +resultado);
+
+
+}
+
+function resta(){
+    var valor1 =10;
+    var valor2 =5;
+    var resultado = valor1 + valor2
+    alert("El resultado de la resta es" -resultado);
+}
+
+function multiplicacion(){
+    var valor1 =10;
+    var valor2 =5;
+    var resultado = valor1 + valor2
+    alert("El resultado de la multiplicacion es" *resultado);
+}
+
+function divicion(){
+    var valor1 =10;
+    var valor2 =5;
+    var resultado = valor1 + valor2
+    alert("El resultado de la divicion es" /resultado);
+}
